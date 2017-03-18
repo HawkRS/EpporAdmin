3fc467fd3811e63f9b1a933ff9ca68859b577b27
HawkRS <ing.carlosglezhdez@hotmail.com>
2017-03-18 09:32:27 +0000

    Cambios en botones de las tareas

--- resources/views/TaskCrud/index.blade.php
+++ resources/views/TaskCrud/index.blade.php
@@ -9,8 +9,8 @@
   <div class="panel panel-default">
     <div class="panel-heading">
       <div class="act-btn">
-          <a class="btn btn-success tbtn mh-25" href="{{ route('TaskCrud.create') }}"> Crear</a>
-          <a class="btn btn-primary tbtn mh-25" href="{{ route('TaskCrud.create') }}">Volver</a>
+          <a class="btn btn-success tbtn mh-45" href="{{ route('TaskCrud.create') }}"> Crear</a>
+          <a class="btn btn-primary tbtn mh-45" href="{{ route('TaskCrud.create') }}">Volver</a>
       </div>
     </div>
     <div class="panel-body">
@@ -24,15 +24,17 @@
       <table class="table table-bordered">
           <tr>
               <th>No</th>
-              <th>Title</th>
-              <th>Description</th>
+              <th>Tarea</th>
+              <th>Creador</th>
+              <th>Comentarios</th>
               <th width="280px">Action</th>
           </tr>
       @foreach ($items as $key => $item)
       <tr>
           <td>{{ ++$i }}</td>
-          <td>{{ $item->title }}</td>
-          <td>{{ $item->description }}</td>
+          <td>{{ $item->tarea }}</td>
+          <td>{{ $item->creador }}</td>
+          <td>{{ $item->comentarios }}</td>
           <td>
               <a class="btn btn-info" href="{{ route('TaskCrud.show',$item->id) }}">Show</a>
               <a class="btn btn-primary" href="{{ route('TaskCrud.edit',$item->id) }}">Edit</a>
