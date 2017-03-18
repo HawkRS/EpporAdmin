cd9127f7414080b001e895f1434cf097bed4eb78
HawkRS <ing.carlosglezhdez@hotmail.com>
2017-03-18 19:48:04 +0000

    Edicion diseño de vista edit de tareas

--- resources/views/TaskCrud/edit.blade.php
+++ resources/views/TaskCrud/edit.blade.php
@@ -1,51 +1,60 @@
 @extends('layouts.app')
 
 @section('content')
-
-    <div class="row">
-        <div class="col-lg-12 margin-tb">
-            <div class="pull-left">
-                <h2>Edit New Item</h2>
-            </div>
-            <div class="pull-right">
-                <a class="btn btn-primary" href="{{ route('TaskCrud.index') }}"> Back</a>
-            </div>
-        </div>
+<section class="main-section">
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <div class="pull-left">
+          <h2>Editar tarea</h2>
+      </div>
+      <div class="clearfix"></div>
     </div>
+    <div class="panel-body">
 
-    @if (count($errors) > 0)
-        <div class="alert alert-danger">
-            <strong>Whoops!</strong> There were some problems with your input.<br><br>
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
+      @if (count($errors) > 0)
+          <div class="alert alert-danger">
+              <strong>Whoops!</strong> There were some problems with your input.<br><br>
+              <ul>
+                  @foreach ($errors->all() as $error)
+                      <li>{{ $error }}</li>
+                  @endforeach
+              </ul>
+          </div>
+      @endif
 
-    {!! Form::model($item, ['method' => 'PATCH','route' => ['TaskCrud.update', $item->id]]) !!}
-    <div class="row">
+      {!! Form::model($item, ['method' => 'PATCH','route' => ['TaskCrud.update', $item->id]]) !!}
+      {{ csrf_field() }}
+      <div class="row">
 
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Title:</strong>
-                {!! Form::text('title', null, array('placeholder' => 'Title','class' => 'form-control')) !!}
+          <div class="col-xs-12 col-sm-12 col-md-12">
+              <div class="form-group">
+                  <strong>Tarea:</strong>
+                  {!! Form::text('tarea', null, array('placeholder' => '$item->tarea','class' => 'form-control')) !!}
+              </div>
+          </div>
+          {{ Form::hidden('creador', Auth::user()->name ) }}
+          <div class="col-xs-12 col-sm-12 col-md-12">
+              <div class="form-group">
+                  <strong>Comentarios:</strong>
+                  {!! Form::textarea('comentarios', null, array('placeholder' => '$item->comentarios','class' => 'form-control','style'=>'height:100px')) !!}
+              </div>
+          </div>
+
+          <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+            <div class="act-btn">
+              <button type="submit" class="btn btn-info tbtn mh-45">Editar</button>
+              <a class="btn btn-primary tbtn mh-45" href="{{ route('TaskCrud.index') }}">Volver</a>
             </div>
-        </div>
+          </div>
 
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Description:</strong>
-                {!! Form::textarea('description', null, array('placeholder' => 'Description','class' => 'form-control','style'=>'height:100px')) !!}
-            </div>
-        </div>
+      </div>{{-- END ROW --}}
 
-        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-                <button type="submit" class="btn btn-primary">Submit</button>
-        </div>
+      {!! Form::close() !!}
 
-    </div>
-    {!! Form::close() !!}
+    </div>{{--END PANEL BODY--}}
+
+  </div>{{--END PANEL--}}
+
+</section>
 
 @endsection
